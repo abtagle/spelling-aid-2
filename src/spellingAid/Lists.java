ae9f0258845856e6c3705f30ad4f484e21285057
atag549 <atag549@xen-rlimage.ece.auckland.ac.nz>
2016-08-17 09:53:06 +1200

    added Lists hopefully singleton class; moved methods/reponsibilities from GUI to WelcomeScreen; provided skeleton for listener to WelcomeScreen buttons

---
+++ src/spellingAid/Lists.java
@@ -0,0 +1,75 @@
+package spellingAid;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+public class Lists {
+	public static final String WORDLIST = "wordlist";
+	public static final String MASTERED = ".mastered";
+	public static final String FAULTED = ".faulted";
+	public static final String FAILED = ".failed";
+	public static final String LAST_FAILED = ".lastFailed";
+	private WordList _wordList;
+	private WordList _mastered;
+	private WordList _faulted;
+	private WordList _failed;
+	private WordList _lastFailed;
+	private static Lists _thisList = null;
+	
+	private Lists(){
+		_thisList = this;
+		_wordList = readInFile(WORDLIST);
+		_mastered = readInFile(MASTERED);
+		_faulted = readInFile(FAULTED);
+		_failed = readInFile(FAILED);
+		_lastFailed = readInFile(LAST_FAILED);
+	}
+	protected static Lists setLists(){
+		if (_thisList == null){
+			return new Lists();
+		} else{
+			return _thisList;
+		}
+	}
+	private WordList readInFile(String filename){
+		WordList words = new WordList();
+		File wordList = new File(filename);
+		if(wordList.exists() || filename.equals(WORDLIST)){
+			try{
+				BufferedReader wordListRead = new BufferedReader(new FileReader(wordList));
+				String word;
+				while((word = wordListRead.readLine()) != null){
+					words.addWord(word);
+				}
+				wordListRead.close();		
+
+			} catch (FileNotFoundException e){
+				JOptionPane.showMessageDialog(null, "Error: unable to load " + filename + ".");
+
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "Error: unable to read from word list " + filename + ".");
+			}
+		}
+		return words;
+	}
+	public WordList getWordList(){
+		return _wordList;
+	}
+	public WordList getMastered(){
+		return _mastered;
+	}
+	public WordList getFaulted(){
+		return _faulted;
+	}
+	public WordList getFailed(){
+		return _failed;
+	}
+	public WordList getLastFailed(){
+		return _lastFailed;
+	}
+}
--- src/spellingAid/options/Quiz.java
+++ src/spellingAid/options/Quiz.java
@@ -1,14 +1,6 @@
 package spellingAid.options;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.util.ArrayList;
-
-import javax.swing.JOptionPane;
-
 import spellingAid.WordList;
 
 public abstract class Quiz {
@@ -17,21 +9,25 @@ public abstract class Quiz {
 	
 	public Quiz(WordList wordlist){
 		_wordlist = wordlist.returnTestlist();
+		startQuiz();
 	}
 	//Acts as template for spelling aloud functionality
-	public void startQuiz(){
+	private void startQuiz(){
 		for(String currentWord : _wordlist){
 			
 		}
 		
 	}
 	
-	public String getName() {
+	private String getName() {
 		return _name;
 	}
 
-	public void setName(String _name) {
+	protected void setName(String _name) {
 		this._name = _name;
 	}
 	//Hook method for spelling aloud implementation
+	private void spellAloud(String word){
+		
+	}
 }
