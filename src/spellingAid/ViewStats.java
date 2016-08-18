2a71f27e81fd2ffc567b476119b9139460d66249
abtagle <abtagle@gmail.com>
2016-08-19 00:26:16 +0100

    Removed unused imports

--- src/spellingAid/ViewStats.java
+++ src/spellingAid/ViewStats.java
@@ -3,8 +3,6 @@ package spellingAid;
 import java.awt.Container;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.TreeSet;
